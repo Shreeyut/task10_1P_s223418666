e729f9abf6821554559bf576b00fa38e3433e5bd
guest1 <guest1@LAPTOP-QEPB68B9>
2024-05-23 19:20:25 +1000

    added mocked tests

--- src/main/java/task_10P/task10/Viewinbox.java
+++ src/main/java/task_10P/task10/Viewinbox.java
@@ -1,4 +1,17 @@
 package task_10P.task10;
 
+
 public class Viewinbox {
+	private ontrackinfo oinfo;
+	
+	public String feedbackstatus() {
+		return oinfo.feedback();
+	}
+	
+	public String messages() {
+		return oinfo.msg();
+	}
+	public void setontrackinfo(ontrackinfo oinfo) {
+		this.oinfo = oinfo;
+	}
 }
--- src/test/java/task_10P/task10/Viewinboxtest.java
+++ src/test/java/task_10P/task10/Viewinboxtest.java
@@ -6,9 +6,27 @@ import org.mockito.Mockito;
 
 
 
+
 public class Viewinboxtest {
 	@Test
 	public void testfeedbackstatus() {
-		Assert.assertEquals("completed", Viewinbox.feedbackstatus());
+		ontrackinfo oninfo = Mockito.mock(ontrackinfo.class);
+		Mockito.when(oninfo.feedback()).thenReturn("completed");
+		
+		Viewinbox views= new Viewinbox();
+		views.setontrackinfo(oninfo);
+		String tutorfeedback = views.feedbackstatus();
+		Assert.assertEquals("completed", tutorfeedback);
+	}
+	
+	@Test
+	public void testviewmessages() {
+		ontrackinfo oninfo = Mockito.mock(ontrackinfo.class);
+		Mockito.when(oninfo.msg()).thenReturn("Well done");
+		
+		Viewinbox views= new Viewinbox();
+		views.setontrackinfo(oninfo);
+		String tutormessages = views.messages();
+		Assert.assertEquals("Well done", tutormessages);
 	}
 }
